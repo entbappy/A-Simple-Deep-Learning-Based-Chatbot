756a2a5f081454112ecff06379ac896232c4b493
entbappy <entbappy73@gmail.com>
2022-07-01 14:55:11 +0600

    notebook added & updated

--- app.py
+++ app.py
@@ -4,8 +4,6 @@ import processor
 
 app = Flask(__name__)
 
-# app.config['SECRET_KEY'] = 'enter-a-very-secretive-key-3479373'
-
 
 @app.route('/', methods=["GET", "POST"])
 def index():
@@ -26,6 +24,5 @@ def chatbotResponse():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port='8888', debug=True)
     app.run(debug=True)
 
